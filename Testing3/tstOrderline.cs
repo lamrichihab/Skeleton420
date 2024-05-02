3133be87bd4002d9feb9d7750fb3528df85ed7a6
Adam Sharp <adam@pearbox.cou.k>
2024-05-02 13:47:06 +0100

    added testing for order class

---
+++ Testing3/tstOrderline.cs
@@ -0,0 +1,17 @@
+﻿using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Testing3
+{
+    [TestClass]
+    public class tstOrderline
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            clsOrderlines AnOrderlines = new clsOrderlines();
+           Assert.IsNotNull(AnOrderlines);
+        }
+    }
+}
