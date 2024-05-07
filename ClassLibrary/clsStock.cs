86742188f1d652257c8ec20eb70c5ea9074a043a
lamrichihab <chihablamri@gmail.com>
2024-05-07 09:33:42 +0100

    tst for Costumeand Stock

---
+++ ClassLibrary/clsStock.cs
@@ -0,0 +1,9 @@
+﻿namespace ClassLibrary
+{
+    public class Stock
+    {
+        public Stock()
+        {
+        }
+    }
+}
---
+++ Testing5/tstStock.cs
@@ -0,0 +1,20 @@
+﻿using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Testing2
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestInstantiationOfStock()
+        {
+            // Act: Instantiate a new Stock object
+            var stock = new Stock(); // Ensure the Stock class exists and has a default constructor
+
+            // Assert: Check if the Stock object is successfully instantiated
+            Assert.IsNotNull(stock, "The Stock object should be instantiated successfully.");
+        }
+    }
+}
