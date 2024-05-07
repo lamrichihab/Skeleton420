254bfef334498dc1f0bce24895eb52e2a8f7d5d9
Mohamed Mekhalfi <P2719088@my365.dmu.ac.uk>
2024-05-07 10:49:42 +0100

    testing for staff and supplier

---
+++ Testing1/tstStaff.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Testing1
+{
+    [TestClass]
+    public class TstStaff
+    {
+        [TestMethod]
+        public void InstanceOk()
+        {
+            // Create an instance of the staff class we want to test
+            clsStaff Staff = new clsStaff();
+
+            // Test to see that it exists
+            Assert.IsNotNull(Staff);
+        }
+    }
+}
