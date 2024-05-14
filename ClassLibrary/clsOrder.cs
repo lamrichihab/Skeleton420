70d06542e54034833564945b5bd276ef6c8a1c7d
Adam Sharp <adam@pearbox.cou.k>
2024-05-14 11:48:54 +0100

    asadasd

--- ClassLibrary/clsOrder.cs
+++ ClassLibrary/clsOrder.cs
@@ -1,25 +1,88 @@
-﻿namespace ClassLibrary
+﻿using System;
+
+namespace ClassLibrary
 {
     public class clsOrder
     {
-        // Primary key for the Order
-        public int OrderId { get; set; }
 
-        // Primary key for the Customer
-        public int CustomerId { get; set; }
 
-        // Primary key for the Employee
-        public int EmployeeId { get; set; }
+        // Private data members for the customer properties
+        private int mOrderId;
+        private int mCustomerId;
+        private int mEmployeeId;
+        private DateTime mOrderDate;
+        private DateTime mRequiredDate;
+        private bool mShipped;
 
-        // Order date
-        public System.DateTime OrderDate { get; set; }
 
-        // Required Order date
-        public System.DateTime RequiredDate { get; set; }
+        // CustomerID public property
+        public int OrderId
+        {
+            get { return mOrderId; }
+            set { mOrderId = value; }
+        }
 
-        public bool Shipped { get; set; }
+        // FullName public property
+        public int CustomerId
+        {
+            get { return mCustomerId; }
+            set { mCustomerId = value; }
+        }
 
-      
-        
+        // EmailAddress public property
+        public int EmployeeId
+        {
+            get { return mEmployeeId; }
+            set { mEmployeeId = value; }
+        }
+
+        // PhoneNumber public property
+        public DateTime OrderDate
+        {
+            get { return mOrderDate; }
+            set { mOrderDate = value; }
+        }
+
+        // ShippingAddress public property
+        public DateTime RequiredDate
+        {
+            get { return mRequiredDate; }
+            set { mRequiredDate = value; }
+        }
+
+
+
+        // IsActive public property
+        public bool Shipped
+        {
+            get { return mShipped; }
+            set { mShipped = value; }
+        }
+
+        // Find method
+        public bool Find(int OrderId)
+        {
+            // For demonstration purposes, let's assume customerID 12 is found
+            if (OrderId == 12)
+            {
+                mOrderId = 12;
+                mCustomerId = 12;
+                mEmployeeId = 12;
+                mOrderDate = new DateTime(2024, 5, 14);
+                mRequiredDate = new DateTime(2024, 5, 24);
+
+                mShipped = true;
+                return true;
+            }
+            else
+            {
+                return false;
+
+
+            }
+        }
+
+       
         }
     }
+
--- Testing6/tstOrder.cs
+++ Testing6/tstOrder.cs
@@ -12,7 +12,7 @@ namespace Testing6
         {
             clsOrder AnOrder = new clsOrder();
             Assert.IsNotNull(AnOrder);
-        
+
         }
         [TestMethod]
         public void OrderIdPropertyOK()
@@ -109,6 +109,161 @@ namespace Testing6
             // Test to see that the two values are the same
             Assert.AreEqual(AnOrder.Shipped, testData);
         }
+        [TestMethod]
+        public void FindMethodOK()
+        {
+            // Create an instance of the class we want to test
+            clsOrder AnOrder = new clsOrder();
 
+            // Create a Boolean variable to store the results of the validation
+            bool found = false;
+
+            // Create some test data to use with the method
+            int OrderId = 12; // Assuming an order with ID 12 exists
+
+            // Invoke the method
+            found = AnOrder.Find(OrderId);
+
+            // Test to see if the result is true
+            Assert.IsTrue(found);
+        }
+        [TestMethod]
+        public void TestOrderIdFound()
+        {
+            // Create an instance of the class we want to test
+            clsOrder AnOrder = new clsOrder();
+
+            // Create a Boolean variable to store the result of the search
+            bool found = false;
+
+            // Create a Boolean variable to record if the data is OK (assume it is)
+            bool ok = true;
+
+            // Create some test data to use with the method
+            int OrderId = 12; // Assuming Order with ID 12 exists
+
+            // Invoke the method
+            found = AnOrder.Find(OrderId);
+
+            // Check the  ID
+            if (AnOrder.OrderId != 12)
+            {
+                ok = false;
+            }
+
+            // Test to see that the result is correct
+            Assert.IsTrue(ok);
+        }
+        [TestMethod]
+        public void TestCustomerIdFound()
+        {
+            // Create an instance of the class we want to create
+            clsOrder AnOrder = new clsOrder();
+            // Create a Boolean variable to store the result of the search
+            bool found = false;
+            // Create a Boolean variable to record if data is OK (assume it is)
+            bool ok = true;
+            // Create some test data to use with the method
+            int OrderId = 12;
+            // Invoke the method
+            found = AnOrder.Find(OrderId);
+            // Check the  property
+            if (AnOrder.OrderId != 12)
+            {
+                ok = false;
+            }
+            // Test to see that the result is correct
+            Assert.IsTrue(ok);
+        }
+
+        [TestMethod]
+        public void TestEmployeeId()
+        {
+            // Create an instance of the class we want to create
+            clsOrder AnOrder = new clsOrder();
+            // Create a Boolean variable to store the result of the search
+            bool found = false;
+            // Create a Boolean variable to record if data is OK (assume it is)
+            bool ok = true;
+            // Create some test data to use with the method
+            int OrderId = 12;
+            // Invoke the method
+            found = AnOrder.Find(OrderId);
+            // Check the  property
+            if (AnOrder.EmployeeId != 12)
+            {
+                ok = false;
+            }
+            // Test to see that the result is correct
+            Assert.IsTrue(ok);
+        }
+
+        [TestMethod]
+        public void TestOrderDateFound()
+        {
+            // Create an instance of the class we want to create
+            clsOrder AnOrder = new clsOrder();
+            // Create a Boolean variable to store the result of the search
+            bool found = false;
+            // Create a Boolean variable to record if data is OK (assume it is)
+            bool ok = true;
+            // Create some test data to use with the method
+            int OrderId = 12;
+            // Invoke the method
+            found = AnOrder.Find(OrderId);
+            // Check the property
+            if (AnOrder.OrderDate != Convert.ToDateTime("14/05/2024"))
+            {
+                ok = false;
+            }
+            // Test to see that the result is correct
+            Assert.IsTrue(ok);
+        }
+
+        [TestMethod]
+        public void TestRequirdDateFound()
+        {
+            // Create an instance of the class we want to create
+            clsOrder AnOrder = new clsOrder();
+            // Create a Boolean variable to store the result of the search
+            bool found = false;
+            // Create a Boolean variable to record if data is OK (assume it is)
+            bool ok = true;
+            // Create some test data to use with the method
+            int OrderId = 12;
+            // Invoke the method
+            found = AnOrder.Find(OrderId);
+            // Check the ContactEmail property
+            if (AnOrder.RequiredDate != Convert.ToDateTime("24/05/2024"))
+            {
+                ok = false;
+            }
+            // Test to see that the result is correct
+            Assert.IsTrue(ok);
+        }
+
+
+
+        [TestMethod]
+        public void TestShippedFound()
+        {
+            // Create an instance of the class we want to create
+            clsOrder AnOrder = new clsOrder();
+            // Create a Boolean variable to store the result of the search
+            bool found = false;
+            // Create a Boolean variable to record if data is OK (assume it is)
+            bool ok = true;
+            // Create some test data to use with the method
+            int OrderId = 12;
+            // Invoke the method
+            found = AnOrder.Find(OrderId);
+            // Check the IsActive property
+            if (AnOrder.Shipped != true)
+            {
+                ok = false;
+            }
+            // Test to see that the result is correct
+            Assert.IsTrue(ok);
+        }
     }
 }
